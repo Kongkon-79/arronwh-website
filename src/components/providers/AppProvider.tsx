e0f661ff50a932bcf8a04de987fa5621d72363cc
Kongkon-79 <kongkonjowarder79@gmail.com>
2026-05-05 15:06:47 +0600

    here section api integration done

--- src/components/providers/AppProvider.tsx
+++ src/components/providers/AppProvider.tsx
@@ -1,15 +1,13 @@
 "use client"
 import React from 'react'
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import { CartProvider } from '../context/cart-context';
 
 const AppProvider = ({ children }: { children: React.ReactNode }) => {
     const queryClient = new QueryClient();
     return (
         <div>
             <QueryClientProvider client={queryClient}>
-                {/* {children} */}
-                 <CartProvider>{children}</CartProvider>
+                {children}
             </QueryClientProvider>
         </div>
     )
